766dd7c35ed07f0524ac8d375a8bcaf4e78088c8
‪Mohammed Mohammed Yousuf‬‏ <mohammed.mo.alkabbani@gmail.com>
2025-08-29 09:41:09 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/wasla/', // Replace 'wasla' with your actual GitHub repository name
+  base: '/Wasla1/', // Replace 'wasla' with your actual GitHub repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
